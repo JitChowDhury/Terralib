b78c1db13be2e6be777539c160b3201712bbcaee
JitChowDhury <jitchowdhury8717@gmail.com>
2025-12-23 13:44:48 +0530

    Add Camera 2d

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,20 @@ int main()
 
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Terralib");
 
+  // CAMERA
+  Camera2D camera;
+  camera.target = Vector2{0.f, 0.f}; // this is what camera follows
+  camera.offset = Vector2{WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
+  // camera.offset = {0.f, 0.f};
+  camera.rotation = 0.0f;
+  camera.zoom = 1.0f;
+
   // GRID LOGIC
 
   int posX{0};
   int posY{0};
   int tileSize{32};
   bool isGridActive{true};
-
-  int gridCountX = WINDOW_WIDTH / tileSize;
-  int gridCountY = WINDOW_HEIGHT / tileSize;
 
   // BLOCKS
   Texture2D tileSet = LoadTexture("assets/sprites/world_tileset.png");
@@ -35,10 +40,28 @@ int main()
 
   while (!WindowShouldClose())
   {
-    Vector2 mousePos = GetMousePosition();
+    if (IsKeyDown(KEY_A))
+      camera.target.x -= 5;
+    if (IsKeyDown(KEY_D))
+      camera.target.x += 5;
+    if (IsKeyDown(KEY_W))
+      camera.target.y -= 5;
+    if (IsKeyDown(KEY_S))
+      camera.target.y += 5;
+
+    Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), camera);
 
     int blockX = (int)(floor(mousePos.x / tileSize) * tileSize);
     int blockY = (int)(floor(mousePos.y / tileSize) * tileSize);
+
+    int tileOnScreenX = (WINDOW_WIDTH / tileSize) + 2;  // 2 for safety check
+    int tileOnScreenY = (WINDOW_HEIGHT / tileSize) + 2; // 2 for safety check
+
+    int startX = (int)(floor(camera.target.x / tileSize) - tileOnScreenX / 2);
+    int startY = (int)(floor(camera.target.y / tileSize) - tileOnScreenY / 2);
+
+    int endX = startX + tileOnScreenX;
+    int endY = startY + tileOnScreenY;
 
     if (IsKeyPressed(KEY_F11))
     {
@@ -55,11 +78,12 @@ int main()
     }
     BeginDrawing();
     ClearBackground(BLACK);
-    // DRAW GRIDS
-    for (int y{0}; y <= gridCountY; y++)
-    {
 
-      for (int x{0}; x <= gridCountX; x++)
+    BeginMode2D(camera);
+    // DRAW GRIDS
+    for (int y{startY}; y < endY; y++)
+    {
+      for (int x{startX}; x < endX; x++)
       {
 
         posX = x * tileSize;
@@ -68,11 +92,11 @@ int main()
         {
           DrawRectangleLines(posX, posY, tileSize, tileSize, WHITE);
         }
-        if (gridCountY - y == height - 1)
+        if (y == height)
         {
           DrawTexturePro(tileSet, grass, Rectangle{(float)posX, (float)posY, (float)tileSize, (float)tileSize}, Vector2{0.f, 0.f}, 0.0f, WHITE);
         }
-        else if (gridCountY - y < height - 1)
+        else if (y > height)
         {
           DrawTexturePro(tileSet, dirt, Rectangle{(float)posX, (float)posY, (float)tileSize, (float)tileSize}, Vector2{0.f, 0.f}, 0.0f, WHITE);
         }
@@ -83,6 +107,7 @@ int main()
     {
       DrawTexturePro(tileSet, dirt, rec, Vector2{0.f, 0.f}, 0.0f, WHITE);
     }
+    EndMode2D();
     EndDrawing();
   }
 }
