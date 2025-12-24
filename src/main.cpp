ce8cf747cfc8ab5e8059d27ae414b0cc8b4fc684
JitChowDhury <jitchowdhury8717@gmail.com>
2025-12-24 09:43:56 +0530

    Add Stone Layer

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main()
   const int WINDOW_HEIGHT{720};
 
   SetTargetFPS(60);
-
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Terralib");
 
   InitAudioDevice();
@@ -26,13 +26,13 @@ int main()
 
   // CAMERA
   Camera2D camera;
-  camera.target = Vector2{0.f, 0.f};                            // this is what camera follows
-  camera.offset = Vector2{WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2}; // camera draws target at offset
+  camera.target = Vector2{0.f, 0.f};                                                  // this is what camera follows
+  camera.offset = Vector2{(float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2}; // camera draws target at offset
   camera.rotation = 0.0f;
   camera.zoom = 1.0f;
 
   // PLAYER
-  Player player(16.f, 6.f, camera.target);
+  Player player(64.f, 16.f, camera.target);
   // GRID LOGIC
 
   int posX{0};
@@ -43,21 +43,25 @@ int main()
   // BLOCKS
   Texture2D grassTileSet = LoadTexture("assets/Terraria_assets/grass.png");
   Texture2D dirtTileSet = LoadTexture("assets/Terraria_assets/Tiles_0.png");
+  Texture2D stoneTileSet = LoadTexture("assets/Terraria_assets/Tiles_9.png");
   Texture2D background = LoadTexture("assets/Terraria_assets/SplashScreens/Splash_9_0.png");
   std::vector<Rectangle> placedBlocks;
   Rectangle grass{18.0f, 0.0f, 16.0f, 16.0f};
   Rectangle dirt{36.0f, 18.0f, 16.0f, 16.0f};
+  Rectangle stone{18.0f, 18.0f, 16.0f, 16.0f};
   SetTextureFilter(grassTileSet, TEXTURE_FILTER_POINT);
   SetTextureFilter(dirtTileSet, TEXTURE_FILTER_POINT);
   SetTextureFilter(background, TEXTURE_FILTER_POINT);
+  SetTextureFilter(stoneTileSet, TEXTURE_FILTER_POINT);
 
   // WORLD GEN
   int height = 5;
+  int grassHeight = 7;
 
   //  MAIN GAME LOOP
   while (!WindowShouldClose())
   {
-
+    std::cout << GetScreenWidth() << std::endl;
     UpdateMusicStream(music);
     player.HandleInput();
     camera.target = player.GetPosition();
@@ -77,20 +81,26 @@ int main()
     int blockY = (int)(floor(mousePos.y / tileSize) * tileSize);
 
     // how many tile can fit on screen + 5 for safety check
-    int tileOnScreenX = (WINDOW_WIDTH / tileSize) + 5;
-    int tileOnScreenY = (WINDOW_HEIGHT / tileSize) + 5;
+    int tileOnScreenX = (GetScreenWidth() / tileSize) + 5;
+    int tileOnScreenY = (GetScreenHeight() / tileSize) + 5;
 
     // start of drawing tile
     // cam.target.x/tilesize means in which grid the target is on then substract half of the tile number
     int startX = (int)(floor(camera.target.x / tileSize) - tileOnScreenX / 2);
     int startY = (int)(floor(camera.target.y / tileSize) - tileOnScreenY / 2);
-    // its that simple
+
     int endX = startX + tileOnScreenX;
     int endY = startY + tileOnScreenY;
 
     if (IsKeyPressed(KEY_F11))
     {
       ToggleBorderlessWindowed();
+      camera.offset = Vector2{(float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2}; // camera draws target at offset
+    }
+
+    if (IsWindowMaximized())
+    {
+      camera.offset = Vector2{(float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2}; // camera draws target at offset
     }
 
     if (IsMouseButtonPressed(0))
@@ -122,19 +132,22 @@ int main()
         {
           DrawTexturePro(grassTileSet, grass, Rectangle{(float)posX, (float)posY, (float)tileSize, (float)tileSize}, Vector2{0.f, 0.f}, 0.0f, WHITE);
         }
-        else if (y > height)
+        else if (y > height && y <= grassHeight)
         {
           DrawTexturePro(dirtTileSet, dirt, Rectangle{(float)posX, (float)posY, (float)tileSize, (float)tileSize}, Vector2{0.f, 0.f}, 0.0f, WHITE);
         }
+        else if (y > grassHeight)
+        {
+          DrawTexturePro(stoneTileSet, stone, Rectangle{(float)posX, (float)posY, (float)tileSize, (float)tileSize}, Vector2{0.f, 0.f}, 0.0f, WHITE);
+        }
       }
     }
-
-    player.Draw();
 
     for (auto &rec : placedBlocks)
     {
       DrawTexturePro(grassTileSet, grass, rec, Vector2{0.f, 0.f}, 0.0f, WHITE);
     }
+    player.Draw();
     EndMode2D();
     EndDrawing();
   }
