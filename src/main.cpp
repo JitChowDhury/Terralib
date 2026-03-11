366704f4d917a82efb70708d514c7a3ffae51b93
JitChowDhury <jitchowdhury8717@gmail.com>
2026-03-11 12:09:18 +0530

    Basic Window

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
 	SetTargetFPS(60);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "Terraria");
 
 	int posX = 30;
@@ -18,6 +19,7 @@ int main()
 		ClearBackground(BLACK);
 
 		DrawRectangle(posX, posY, size, size, BLUE);
+		DrawText("My game", 100, 200,26, RED);
 		posX += 1;
 		EndDrawing();
 	}
