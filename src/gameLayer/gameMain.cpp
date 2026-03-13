926110e71e9a7eabaaa3c315539703286b3b0e60
JitChowDhury <jitchowdhury8717@gmail.com>
2026-03-13 09:22:06 +0530

    deltaTime

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include "gameMain.h"
 
+struct GameData
+{
+	float positionX = 100;
+	float positionY = 100;
+}gameData;
+
+
 bool initGame()
 {
 	return true;
@@ -9,8 +16,18 @@ bool initGame()
 
 bool updateGame()
 {
+	float deltaTime = GetFrameTime();
 
-	DrawText("My game", 100, 200, 26, RED);
+	if (deltaTime > 1.f / 5) { deltaTime = 1.f / 5; }
+
+	//DrawText("My game", 100, 200, 26, RED);
+
+	if (IsKeyDown(KEY_A)) { gameData.positionX -= 100 * deltaTime; }
+	if (IsKeyDown(KEY_D)) { gameData.positionX += 100 * deltaTime; }
+	if (IsKeyDown(KEY_W)) { gameData.positionY -= 100 * deltaTime; }
+	if (IsKeyDown(KEY_S)) { gameData.positionY += 100 * deltaTime; }
+
+	DrawRectangle(gameData.positionX, gameData.positionY, 80, 80, {172,67,235,255});
 	return true;
 }
 
