6db8a1ce4e7e9e050ba8a33122c930811ebc57e6
JitChowDhury <jitchowdhury8717@gmail.com>
2026-03-13 12:06:18 +0530

    Assetmanager

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,16 +1,20 @@
 #include <raylib.h>
 #include <iostream>
 #include "gameMain.h"
+#include "assetManager.h"
+
+
 
 struct GameData
 {
-	float positionX = 100;
-	float positionY = 100;
+
 }gameData;
 
-
+AssetManager assetManager;
+ 
 bool initGame()
 {
+	assetManager.loadAll();
 	return true;
 }
 
@@ -21,13 +25,7 @@ bool updateGame()
 	if (deltaTime > 1.f / 5) { deltaTime = 1.f / 5; }
 
 	//DrawText("My game", 100, 200, 26, RED);
-
-	if (IsKeyDown(KEY_A)) { gameData.positionX -= 100 * deltaTime; }
-	if (IsKeyDown(KEY_D)) { gameData.positionX += 100 * deltaTime; }
-	if (IsKeyDown(KEY_W)) { gameData.positionY -= 100 * deltaTime; }
-	if (IsKeyDown(KEY_S)) { gameData.positionY += 100 * deltaTime; }
-
-	DrawRectangle(gameData.positionX, gameData.positionY, 80, 80, {172,67,235,255});
+	DrawTexturePro(assetManager.dirt, { 0,0,(float)assetManager.dirt.width,(float)assetManager.dirt.height }, { 100,100,50,50 }, {}, 0, WHITE);
 	return true;
 }
 
