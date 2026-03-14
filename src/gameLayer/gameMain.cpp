f1a03c8934407b5a7095f759570907e0b49aef42
JitChowDhury <jitchowdhury8717@gmail.com>
2026-03-14 13:49:20 +0530

    GameMap

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include "gameMain.h"
 #include "assetManager.h"
+#include "gameMap.h"
 
 
 
 struct GameData
 {
-
+	GameMap gameMap;
 }gameData;
 
 AssetManager assetManager;
@@ -15,17 +16,50 @@ AssetManager assetManager;
 bool initGame()
 {
 	assetManager.loadAll();
+	gameData.gameMap.create(30, 10);
+
+	gameData.gameMap.getBlockUnsafe(0, 0).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(1, 1).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(2, 2).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(3, 3).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(4, 4).type = Block::dirt;
+
 	return true;
 }
 
 bool updateGame()
 {
 	float deltaTime = GetFrameTime();
-
 	if (deltaTime > 1.f / 5) { deltaTime = 1.f / 5; }
 
+	ClearBackground(BLACK);
+
+	for (int y = 0; y < gameData.gameMap.h; y++)
+	{
+		for (int x = 0; x < gameData.gameMap.w; x++)
+		{
+			auto& b = gameData.gameMap.getBlockUnsafe(x, y);
+
+			if (b.type != Block::air)
+			{
+				float size = 32;
+				float posX = x * size;
+				float posY = y * size;
+
+				DrawTexturePro(
+					assetManager.dirt,
+					Rectangle{ 0.f,0.f,(float)assetManager.dirt.width,(float)assetManager.dirt.height },
+					{ posX,posY,size,size },
+					{ 0,0 },
+					0.0f,
+					WHITE
+				);
+			}
+		}
+	}
+
 	//DrawText("My game", 100, 200, 26, RED);
-	DrawTexturePro(assetManager.dirt, { 0,0,(float)assetManager.dirt.width,(float)assetManager.dirt.height }, { 100,100,50,50 }, {}, 0, WHITE);
+
 	return true;
 }
 
